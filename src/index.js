1954d54a966ff67006c07658392e2b68e6978604
yuki-4910 <yokuto.ishii@nleats.com>
2021-04-03 19:17:25 -0230

    small change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ColorModeScript } from "@chakra-ui/react"
 import theme from "./them"
 
 ReactDOM.render(
